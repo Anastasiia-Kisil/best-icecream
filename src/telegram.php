ed7fdea48417a33a5bd043328291e8282ba0ef13
Anastasiia kisil <hunterr25n@gmail.com>
2021-04-24 21:54:25 +0400

    bot

---
+++ src/telegram.php
@@ -0,0 +1,38 @@
+<?php
+ 
+/* https://api.telegram.org/bot1612770931:AAFiKG4YZny1E2OiwlR1IJYgjROzIjIWTiQ/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+ 
+//Переменная $name,$phone, $mail получает данные при помощи метода POST из формы
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+ 
+//в переменную $token нужно вставить токен, который нам прислал @botFather
+$token = "1612770931:AAFiKG4YZny1E2OiwlR1IJYgjROzIjIWTiQ";
+ 
+//нужна вставить chat_id (Как получить chad id, читайте ниже)
+$chat_id = "-309087490";
+ 
+//Далее создаем переменную, в которую помещаем PHP массив
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+ 
+//При помощи цикла перебираем массив и помещаем переменную $txt текст из массива $arr
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+ 
+//Осуществляется отправка данных в переменной $sendToTelegram
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+ 
+//Если сообщение отправлено, напишет "Thank you", если нет - "Error"
+if ($sendToTelegram) {
+  echo "Thank you";
+} else {
+  echo "Error";
+}
+?>
