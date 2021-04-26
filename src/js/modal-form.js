99253c0b4f8be406801d5ee58f58a8309563df1c
Andrew Yatsutko <eitherwiser@gmail.com>
2021-04-26 03:22:27 +0300

    modal-form.js fix for mobile menu \ mobile-menu and header markup

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -1,12 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-form-open]'),
+    openModalMenuBtn: document.querySelector('[menu-data-modal-form-open]'),
     closeModalBtn: document.querySelector('[data-modal-form-close]'),
     modal: document.querySelector('[data-modal-form]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalMenuBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
