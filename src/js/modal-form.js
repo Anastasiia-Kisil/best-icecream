9aebca7d67ec6bf0c322c808aff243a3e1c68d3d
Andrew Yatsutko <eitherwiser@gmail.com>
2021-04-26 03:43:29 +0300

    body owerflow block wile modal shown

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -4,6 +4,7 @@
     openModalMenuBtn: document.querySelector('[menu-data-modal-form-open]'),
     closeModalBtn: document.querySelector('[data-modal-form-close]'),
     modal: document.querySelector('[data-modal-form]'),
+    bodyOverflow: document.querySelector('[overflow-body]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -13,5 +14,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.bodyOverflow.classList.toggle('overflow--blocked');
   }
 })();
