5cb3604d6f935c3976ddcf7720f78beeaa57570e
DyPeBaP <66387850+DyPeBaP@users.noreply.github.com>
2021-04-26 23:19:26 +0300

    В html включил скрол (скрипт), добавил фото и ховет.

--- src/js/scrolling.js
+++ src/js/scrolling.js
@@ -6,13 +6,3 @@ scrollTop: $($.attr(this, 'href')).offset().top
 }, 550);
 return false;
 });
-
-// скрипт прокрутки страницы вверх
-function up() {  
-var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);  
-if(top > 0) {  
-window.scrollBy(0,((top+400)/-40));  
-t = setTimeout('up()',5);  
-} else clearTimeout(t);  
-return false;  
-}
