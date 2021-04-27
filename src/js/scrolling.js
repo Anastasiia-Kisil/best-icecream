9e266e9ad036030175ac5264ea1d218e31474aad
Andrew Yatsutko <eitherwiser@gmail.com>
2021-04-27 03:44:55 +0300

    URGENT (refactored scrolling.js)! for mobile-menu, it is hide on click now (refactored scrolling.js) \  refactored markup fo mobile menu \ fixed burger in header

--- src/js/scrolling.js
+++ src/js/scrolling.js
@@ -1,18 +1,26 @@
 // скролл к якорю jquery
 var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-$page.animate({
-scrollTop: $($.attr(this, 'href')).offset().top
-}, 550);
-return false;
+$('a[href*="#"]').on("click", function () {
+
+  $menuVisibility = $('.mobile-menu'), // ищет класс .mobile-menu
+    $bodyOverflow = $('body'); // идёт к body
+
+  $page.animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 550);
+
+  $menuVisibility.addClass('menu--hidden'), // добавляет класс menu--hidden
+    $bodyOverflow.removeClass('overflow--blocked');  // удаляет класс menu--hidden
+
+  return false;
 });
 
 // скрипт прокрутки страницы вверх
-function up() {  
-var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);  
-if(top > 0) {  
-window.scrollBy(0,((top+400)/-40));  
-t = setTimeout('up()',5);  
-} else clearTimeout(t);  
-return false;  
+function up() {
+  var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+  if (top > 0) {
+    window.scrollBy(0, ((top + 400) / -40));
+    t = setTimeout('up()', 5);
+  } else clearTimeout(t);
+  return false;
 }
