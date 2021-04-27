063a7776f089b19c96d65f8bc63115826276fe47
Anastasiia kisil <hunterr25n@gmail.com>
2021-04-27 12:54:10 +0400

    clean smth

--- src/js/scrolling.js
+++ src/js/scrolling.js
@@ -16,13 +16,3 @@ $('a[href*="#"]').on('click', function () {
 
   return false;
 });
-
-//  скрипт прокрутки страницы вверх
-function up() {
-  var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-  if (top > 0) {
-    window.scrollBy(0, (top + 400) / -40);
-    t = setTimeout('up()', 5);
-  } else clearTimeout(t);
-  return false;
-}
